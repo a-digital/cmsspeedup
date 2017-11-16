95c66c6bc9ac7b81c8819853f619d7a10ed65480
Matt Shearing <matt@adigital.co.uk>
2017-11-16 16:18:00 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# CMS Speed Up plugin for Craft CMS
+
+Performs a CMS Speed Up after an update is completed
+
+![Screenshot](resources/screenshots/plugin_logo.png)
+
+## Installation
+
+To install CMS Speed Up, follow these steps:
+
+1. Download & unzip the file and place the `cmsspeedup` directory into your `craft/plugins` directory
+2.  -OR- do a `git clone ???` directly into your `craft/plugins` folder.  You can then update it with `git pull`
+3. Install plugin in the Craft Control Panel under Settings > Plugins
+4. The plugin folder should be named `cmsspeedup` for Craft to see it.  GitHub recently started appending `-master` (the branch name) to the name of the folder for zip file downloads.
+
+CMS Speed Up works on Craft 2.4.x and Craft 2.5.x.
+
+## CMS Speed Up Overview
+
+-Insert text here-
+
+## Configuring CMS Speed Up
+
+-Insert text here-
+
+## Using CMS Speed Up
+
+-Insert text here-
+
+## CMS Speed Up Roadmap
+
+Some things to do, and ideas for potential features:
+
+* Release it
+
+## CMS Speed Up Changelog
+
+### 1.0.0 -- 2017.11.16
+
+* Initial release
+
+Brought to you by [Matt Shearing](adigital.agency)
---
+++ services/CmsSpeedUpService.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * CMS Speed Up plugin for Craft CMS
+ *
+ * CMS Speed Up Service
+ *
+ * --snip--
+ * All of your plugin’s business logic should go in services, including saving data, retrieving data, etc. They
+ * provide APIs that your controllers, template variables, and other plugins can interact with.
+ *
+ * https://craftcms.com/docs/plugins/services
+ * --snip--
+ *
+ * @author    Matt Shearing
+ * @copyright Copyright (c) 2017 Matt Shearing
+ * @link      adigital.agency
+ * @package   CMS Speed Up
+ * @since     1.0.0
+ */
+
+namespace Craft;
+
+class CmsSpeedUpService extends BaseApplicationComponent
+{
+    /**
+     * This function can literally be anything you want, and you can have as many service functions as you want
+     *
+     * From any other plugin file, call it like this:
+     *
+     *     craft()->cmsspeedup->copyResources()
+     */
+    public function copyResources()
+    {
+	    $src = craft()->path->getAppPath()."resources";
+	    $dest = $_SERVER["DOCUMENT_ROOT"]."/".craft()->config->get('cpTrigger')."/resources";
+	    if (is_dir($dest)) {
+		    rmdir($dest);
+	    }
+	    $this->recurseCopy($src, $dest);
+	    return true;
+    }
+    
+    public function recurseCopy($src, $dst) {
+	    $dir = opendir($src);
+	    @mkdir($dst);
+	    while(false !== ($file = readdir($dir))) {
+	        if ($file != '.' && $file != '..') {
+	            if (is_dir($src.'/'.$file)) {
+	                $this->recurseCopy($src.'/'.$file, $dst.'/'.$file);
+	            } else {
+	                copy($src.'/'.$file, $dst.'/'.$file);
+	            }
+	        }
+	    }
+	    closedir($dir);
+	} 
+
+}
